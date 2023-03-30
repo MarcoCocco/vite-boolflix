95d05b0e8b019bb6be660a1ba6fb8752fda5302b
MarcoCocco <marcococco28@gmail.com>
2023-03-30 16:04:49 +0200

    fix: aggiunta una nuova Promise alla funzione

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { store } from './store.js';
 
 export default {
+
   data() {
     return {
 
@@ -20,32 +21,39 @@ export default {
 
   methods: {
 
+    //Creo una funzione che mi permette di costruire una chiamata API di ricerca di film o serieTV in base ai parametri inseriti.
+    //Il risultato della chiamata verrà trasferita negli array vuoti di movies e series contenuti in store grazie al primo parametro specificato.
     getAPIcallMovieSeries(searchOption, searchByName) {
+      //Aggiungo una Promise all'interno della funzione per risolvere la chiamata prima di accedere ai dati, così da poterli avere solo quando sono disponibili.
+      return new Promise((resolve) => {
+        axios.get(`${this.store.APIcall}search/${searchOption + this.store.APIkey}&query=${searchByName}`).then((res) => {
 
-      axios.get(`${this.store.APIcall}search/${searchOption + this.store.APIkey}&query=${searchByName}`).then((res) => {
+          if (searchOption === 'movie') {
+            this.store.movies = res.data.results;
+          };
 
-        if (searchOption === 'movie') {
-          this.store.movies = res.data.results;
-        };
+          if (searchOption === 'tv') {
+            this.store.series = res.data.results;
+          };
 
-        if (searchOption === 'tv') {
-          this.store.series = res.data.results;
-        };
+          resolve();
 
+        });
       });
-
-    }
+    },
 
   },
 
   created() {
-    this.getAPIcallMovieSeries('', '');
-    console.log(this.store.series);
-    console.log(this.store.movies);
+    //Richiamo la funzione di chiamata API inserendo i parametri specificati.
+    this.getAPIcallMovieSeries('', '').then(() => {
+      console.log(this.store.series);
+      console.log(this.store.movies);
+    });
   },
 
-
 }
+  
 </script>
 
 <template>
